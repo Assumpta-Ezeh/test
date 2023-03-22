1268638d5e52d329d55644dcb99baae49b9be939
Assumpta  Tech <127113121+Assumpta-Ezeh@users.noreply.github.com>
2023-03-22 21:51:17 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,47 @@
+const dog1 = document.getElementById("dog1");
+dog1.addEventListener("click", (event) => {
+    event.target.style.background = "yellow"
+    event.target.style.color = "black"
+})
+
+const dog2 = document.getElementById("dog2");
+dog2.addEventListener("click", (event) => {
+    event.target.style.background = "black"
+    event.target.style.color = "white"
+})
+
+const stage = document.getElementById("stage");
+stage.addEventListener("click", (event) => {
+    event.target.style.background = "purple"
+    event.target.style.color = "yellow"
+})
+
+const mountain = document.getElementById("mountain");
+mountain.addEventListener("click", (event) => {
+    event.target.style.background = "blue"
+    event.target.style.color = "white"
+})
+
+const card1 = document.getElementById("card1");
+card1.addEventListener("click", (event) => {
+    event.target.style.background = "orange"
+    event.target.style.color = "blue"
+})
+
+const card2 = document.getElementById("card2");
+card2.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+})
+
+const dog3 = document.getElementById("dog3");
+dog3.addEventListener("click", (event) => {
+    event.target.style.background = "green"
+    event.target.style.color = "yellow"
+})
+
+const darkpurple = document.getElementById("darkpurple");
+darkpurple.addEventListener("click", (event) => {
+    event.target.style.background = "pink"
+    event.target.style.color = "black"
+})
